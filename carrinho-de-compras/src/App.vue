24baeebe5b4b928d486c751c1a44de0a759789fa
Laíssa Mendes <laissa.mendes2006@gmail.com>
2023-05-05 11:55:25 -0300

    produtos adicionados

--- carrinho-de-compras/src/App.vue
+++ carrinho-de-compras/src/App.vue
@@ -1,47 +1,81 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const produtos_mari = [
+  {
+    id: 1,
+    nome: 'GingerGlow - Nude',
+    preco: 85.0
+  },
+  {
+    id: 2,
+    nome: 'GingerGlow - Color',
+    preco: 85.0
+  },
+  {
+    id: 3,
+    nome: 'Base - CoverUp',
+    preco: 40.0
+  }
+]
+const produtos_bt = [
+  {
+    id: 1,
+    nome: 'Base BT Skin',
+    preco: 70.0
+  },
+  {
+    id: 2,
+    nome: 'Iluminador - BT Mirror',
+    preco: 65.0
+  },
+  {
+    id: 3,
+    nome: 'BT Elixir Facial Acqua Hydra',
+    preco: 60.0
+  }
+]
+const produtos_fran = [
+  {
+    id: 1,
+    nome: 'Gloss Labial Pink Chilli',
+    preco: 49.9
+  },
+  {
+    id: 2,
+    nome: 'Stick Balms By Fran',
+    preco: 39.9
+  },
+  {
+    id: 3,
+    nome: 'Máscara Para Cílios Fran Maravicherry',
+    preco: 55.0
+  }
+]
+const produtos_bocaRosa = [
+  {
+    id: 1,
+    nome: 'Pó Boca Rosa Beauty',
+    preco: 49.9
+  },
+  {
+    id: 2,
+    nome: 'Boca Rosa by Payot Tint',
+    preco: 39.9
+  },
+  {
+    id: 3,
+    nome: 'Corretivo Líquido - Boca Rosa',
+    preco: 40.0
+  }
+]
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    {{  produtos_bt }}
+    {{ produtos_mari }}
+    {{  produtos_bocaRosa }}
+    {{ produtos_fran }}
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
