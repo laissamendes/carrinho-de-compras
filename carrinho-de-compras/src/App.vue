1960343a34fefc0ca7730edf5c2fa531539f2575
Laíssa Mendes <laissa.mendes2006@gmail.com>
2023-05-09 11:32:25 -0300

    comit do layout e ajuste das imagens

--- carrinho-de-compras/src/App.vue
+++ carrinho-de-compras/src/App.vue
@@ -1,165 +1,197 @@
 <script setup>
-
 import { ref, computed } from 'vue'
 
 const produtos_mari = [
   {
     id: 1,
     nome: 'GingerGlow - Nude',
-    preco: 85.0 , 
-    quantidade: 0
+    preco: 85.0,
+    quantidade: 0,
+    imagem: 'paleta-1.png'
   },
   {
     id: 2,
     nome: 'GingerGlow - Color',
-    preco: 85.0 , 
-    quantidade: 0
+    preco: 85.0,
+    quantidade: 0,
+    imagem: 'paleta-2.png'
   },
   {
     id: 3,
     nome: 'Base - CoverUp',
-    preco: 40.0 , 
-    quantidade: 0
+    preco: 40.0,
+    quantidade: 0,
+    imagem: 'base-coverup.png'
   }
 ]
 const produtos_bt = [
   {
     id: 1,
     nome: 'Base BT Skin',
-    preco: 70.0 , 
-    quantidade: 0
+    preco: 70.0,
+    quantidade: 0,
+    imagem: 'base-bt.jpg'
   },
   {
     id: 2,
     nome: 'Iluminador - BT Mirror',
-    preco: 65.0 , 
-    quantidade: 0
+    preco: 65.0,
+    quantidade: 0,
+    imagem: 'iluminador-bt.png'
   },
   {
     id: 3,
     nome: 'BT Elixir Facial Acqua Hydra',
-    preco: 60.0 , 
-    quantidade: 0
+    preco: 60.0,
+    quantidade: 0,
+    imagem: 'primer-bt.jpeg'
   }
 ]
 const produtos_fran = [
   {
     id: 1,
     nome: 'Gloss Labial Pink Chilli',
-    preco: 49.9 , 
-    quantidade: 0
+    preco: 49.9,
+    quantidade: 0,
+    imagem: 'pink-chily.jpeg'
   },
   {
     id: 2,
     nome: 'Stick Balms By Fran',
-    preco: 39.9 , 
-    quantidade: 0
+    preco: 39.9,
+    quantidade: 0,
+    imagem: 'balm-fran.jpeg'
   },
   {
     id: 3,
     nome: 'Máscara Para Cílios Fran Maravicherry',
-    preco: 55.0 , 
-    quantidade: 0
+    preco: 55.0,
+    quantidade: 0,
+    imagem: 'mascara-fran.jpeg'
   }
 ]
 const produtos_bocaRosa = [
   {
     id: 1,
     nome: 'Pó Boca Rosa Beauty',
-    preco: 49.9 , 
-    quantidade: 0
+    preco: 49.9,
+    quantidade: 0,
+    imagem: 'po-bocarosa.jpeg'
   },
   {
     id: 2,
     nome: 'Boca Rosa by Payot Tint',
-    preco: 39.9 , 
-    quantidade: 0
+    preco: 39.9,
+    quantidade: 0,
+    imagem: 'corretivo-bocarosa.jpg'
   },
   {
     id: 3,
     nome: 'Corretivo Líquido - Boca Rosa',
-    preco: 40.0 , 
-    quantidade: 0
+    preco: 40.0,
+    quantidade: 0,
+    imagem: 'boca-rosa-tint.jpeg'
   }
 ]
-const mostrar = ref(true)
+//*const mostrar = ref(true)
 
-function incrementar() {
-  quantidade.value++
-}
-function decrementar(){
-  quantidade.value--
-}
-function resetar() {
- quantidade.value = 0
-}
-const soma = computed(() => {
-    return contador1.value + contador2.value
-})
+//function incrementar() {
+//quantidade.value++
+//}
+//function decrementar(){
+// quantidade.value--
+//}
+//function resetar() {
+//quantidade.value = 0
+//}
+//const soma = computed(() => {
+//    return contador1.value + contador2.value
+//})
 </script>
 
 <template>
   <div id="loja-completa">
     <h1>Mendes MakeUp</h1>
-  <div id="produtos">
-<div id="produtos-Mari">
-  <h2>Marimaria MakeUp</h2>
-    <div v-for="(produto, index) in produtos_mari" :key="index" >  {{ produto.id }} {{  produto.nome }} {{ quantidade }}
-      
-      <button class="btn-adicionar" @click="incrementar">+</button>
-      <button class="btn-decrmentar" @click="decrementar">-</button>
-      <button class="btn-deletar" @click="resetar">C</button>
-      <p>Quantidade de produto(s): {{ quantidade }}</p>
+    <div id="produtos">
+      <div id="produtos-Mari" >
+        <h2>Marimaria MakeUp</h2>
+        <div class="marcas">
+          <div v-for="(produto, index) in produtos_mari" :key="index">
+          {{ produto.id }} {{ produto.nome }}
+          <img :src="produto.imagem" alt="" width="200" />
+          <button class="btn-adicionar" @click="incrementar">+</button>
+          <img src="lixeira.png" alt="" width="25" class="btn-deletar" @click="resetar" />
+          <button class="btn-decrmentar" @click="decrementar">-</button>
+
+          <p>Quantidade de produto(s): {{ quantidade }}</p>
+        </div>
+        </div>
+        
+      </div>
+      <div id="produtos-BT" >
+        <h2>Bruna Tavares</h2>
+        <div class="marcas">
+          <div v-for="(produto, index) in produtos_bt" :key="index">
+          {{ produto.id }} {{ produto.nome }}
+          <img :src="produto.imagem" alt="" width="200" />
+          <button class="btn-adicionar" @click="incrementar">+</button>
+          <img src="lixeira.png" alt="" width="25" class="btn-deletar" @click="resetar" />
+          <button class="btn-decrmentar" @click="decrementar">-</button>
+          <p>Quantidade de produto(s): {{ contador }}</p>
+        </div>
+        </div>
+        
+      </div>
+      <div id="produtos-Fran" >
+        <h2>Fran</h2>
+        <div class="marcas">
+          <div v-for="(produto, index) in produtos_fran" :key="index" >
+          {{ produto.id }} {{ produto.nome }}
+          <img :src="produto.imagem" alt="" width="200" />
+          <button class="btn-adicionar" @click="incrementar">+</button>
+          <img src="lixeira.png" alt="" width="25" class="btn-deletar" @click="resetar" />
+          <button class="btn-decrmentar" @click="decrementar">-</button>
+
+          <p>Quantidade de produto(s): {{ contador }}</p>
+        </div>
+        </div>
+        
+      </div>
+      <div id="produtos-bocaRosa">
+        <h2>Boca Rosa Beauty</h2>
+<div class="marcas"><div v-for="(produto, index) in produtos_bocaRosa" :key="index" >
+          {{ produto.id }} {{ produto.nome }}
+          <img :src="produto.imagem" alt="" width="200" />
+          <button class="btn-adicionar" @click="incrementar">+</button>
+          <img src="lixeira.png" alt="" width="25" class="btn-deletar" @click="resetar" />
+          <button class="btn-decrmentar" @click="decrementar">-</button>
+
+          <p>Quantidade de produto(s): {{ produto.quantidade }}</p>
+        </div></div>
+        
+      </div>
     </div>
-    <img src="paleta-1.png" alt="" width="250">
-    <img src="paleta-2.png" alt="" width="250">
-    <img src="base-coverup.png" alt="" width="250">
-</div>
-<div id="produtos-BT">
-  <h2>Bruna Tavares</h2>
-  <div v-for="(produto, index) in produtos_bt" :key="index" >  {{ produto.id }} {{  produto.nome }} {{ quantidade }}
-      <button class="btn-adicionar" @click="incrementar">+</button>
-      <button class="btn-decrmentar" @click="decrementar">-</button>
-      <button class="btn-deletar" @click="resetar">C</button>
-      <p>Quantidade de produto(s): {{ contador }}</p>
-      <img src="base-bt.jpg" alt="" width="250">
-      <img src="iluminador-bt.png" alt="" width="250">
-      <img src="primer-bt.jpeg" alt="" width="250">
-    </div>
-  </div>
-<div id="produtos-Fran">
-  <h2>Fran</h2>
-  <div v-for="(produto, index) in produtos_fran" :key="index" >  {{ produto.id }}  {{  produto.nome }} {{ quantidade }}
-      <button class="btn-adicionar" @click="incrementar">+</button>
-      <button class="btn-decrmentar" @click="decrementar">-</button>
-      <button class="btn-deletar" @click="resetar"><img src="lixeira.png" alt=""></button>
-      <img src="balm-fran.jpeg" alt="" width="250">
-      <img src="mascara-fran.jpeg" alt="" width="250">
-      <img src="pink-chily.jpeg" alt="" width="250">
-      <p>Quantidade de produto(s): {{ contador }}</p>
-    </div>
-</div>
-<div id="produtos-bocaRosa">
-<h2>Boca Rosa Beauty</h2>
-  <div v-for="(produto, index) in produtos_bocaRosa" :key="index" >  {{ produto.id }} {{  produto.nome }} {{ quantidade }}
-      <button class="btn-adicionar" @click="incrementar">+</button>
-      <button class="btn-decrmentar" @click="decrementar">-</button>
-      <button class="btn-deletar" @click="resetar">C</button>
-      <p>Quantidade de produto(s): {{ contador }}</p>
-      <img src="po-bocarosa.jpeg" alt="" width="250">
-      <img src="corretivo-bocarosa.jpg" alt="" width="250">
-      <img src="boca-rosa-tint.jpeg" alt="" width="250">
-    </div>
-</div>
-  </div>
-  </div>
-  
-  <div>
-    {{  produtos_bt }}
-    {{ produtos_mari }}
-    {{  produtos_bocaRosa }}
-    {{ produtos_fran }}
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.marcas {
+  display: flex;
+  justify-content: space-around;
+}
+
+h1 {
+  text-align: center;
+  font-family: 'URW Chancery L, cursive';
+  color: rgb(255, 0, 98);
+  text-shadow: 5px 5px 5px rgba(0, 0, 0, 0.623);
+}
+h2 {
+  text-align: left;
+  color: rgb(255, 68, 140);
+  margin: 5px;
+}
+p {
+  text-align: center;
+}
+</style>
